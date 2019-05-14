107389c204bbae8a6c692b2eb3984b370adebe6f
AWSafran <andrewsafran@gmail.com>
2019-05-14 15:26:06 -0400

    made find all students pageable

--- school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
@@ -4,6 +4,7 @@ import com.lambdaschool.school.model.Course;
 import com.lambdaschool.school.repository.CourseRepository;
 import com.lambdaschool.school.view.CountStudentsInCourses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
